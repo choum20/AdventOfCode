0a5cfb32e5ef94c17d7b36fedb95b24be22416e5
Arnaud <admin@ravenrock.be>
2023-12-12 22:53:38 +0100

    Celui vient du web. Trop casse tête

--- 12/Aoc12/Aoc12/Etape1.cs
+++ 12/Aoc12/Aoc12/Etape1.cs
@@ -8,9 +8,55 @@ namespace Aoc12
 {
 	internal static class Etape1
 	{
-		internal static string Execute()
+		internal static long Ways(string pattern, int[] counts)
 		{
-			return "";
+			return Ways(pattern, counts, 0, 0, new Dictionary<(int, int), long>());
+		}
+
+		static long Ways(string pattern, int[] counts, int patternIndex, int countIndex, Dictionary<(int, int), long> waysMemo)
+		{
+			var memoKey = (patternIndex, countIndex);
+			if (waysMemo.TryGetValue(memoKey, out var memo))
+			{
+				return memo;
+			}
+
+			if (patternIndex >= pattern.Length)
+			{
+				return countIndex == counts.Length ? 1 : 0;
+			}
+
+			long ways = 0;
+
+			var currentPatternChar = pattern[patternIndex];
+			if (currentPatternChar != '#') // either . so we skip it, or ? so consider what if we do skip it
+			{
+				ways += Ways(pattern, counts, patternIndex + 1, countIndex, waysMemo);
+			}
+
+			if (currentPatternChar != '.' && countIndex < counts.Length) // either # so we consume it, or ? so consider what if we do consume it
+			{
+				var enoughToConsume = true;
+				var neededCount = counts[countIndex] - 1; // we've already got the first
+				while (enoughToConsume && neededCount > 0)
+				{
+					patternIndex++;
+					neededCount--;
+					enoughToConsume = patternIndex < pattern.Length && pattern[patternIndex] != '.';
+				}
+
+				if (enoughToConsume)
+				{
+					var separatorIndex = patternIndex + 1;
+					if (separatorIndex >= pattern.Length || pattern[separatorIndex] != '#')
+					{
+						ways += Ways(pattern, counts, separatorIndex + 1, countIndex + 1, waysMemo);
+					}
+				}
+			}
+
+			waysMemo.Add(memoKey, ways);
+			return ways;
 		}
 	}
 }
--- 12/Aoc12/Aoc12/Program.cs
+++ 12/Aoc12/Aoc12/Program.cs
@@ -1,10 +1,18 @@
 ﻿using Aoc12;
+using System.Linq;
 
 var donnees = System.IO.File.ReadAllLines("./Data.txt");
 
+var split = donnees.Select(x => x.Split(new[] { ' ' },StringSplitOptions.None));
 
-
-var reponse1 = Etape1.Execute();
+long reponse1 = 0;
+foreach (var item in split)
+{
+	var pattern = $"{item.First()}?{item.First()}?{item.First()}?{item.First()}?{item.First()}";
+	var counts = item.Last().Split(',').Select(x => int.Parse(x)).ToArray();
+	counts = counts.Concat(counts).Concat(counts).Concat(counts).Concat(counts).ToArray();
+	reponse1 += Etape1.Ways(pattern, counts);
+}
 Console.WriteLine(reponse1);
 
 var reponse2 = Etape2.Execute();
