40e4e9972f6c27f8a308797b38c04d8924ed35fd
Arnaud <admin@ravenrock.be>
2023-12-12 21:38:37 +0100

    Init Jour 12

---
+++ 12/Aoc12/Aoc12/Etape2.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aoc12
+{
+	internal static class Etape2
+	{
+		internal static string Execute()
+		{
+			return "";
+		}
+	}
+}
---
+++ 12/Aoc12/Aoc12/Program.cs
@@ -0,0 +1,13 @@
+﻿using Aoc12;
+
+var donnees = System.IO.File.ReadAllLines("./Data.txt");
+
+
+
+var reponse1 = Etape1.Execute();
+Console.WriteLine(reponse1);
+
+var reponse2 = Etape2.Execute();
+Console.WriteLine(reponse2);
+
+Console.ReadLine();
